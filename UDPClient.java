1d6ec7cd80f24ee254edddb7c17e87089446eca3
silver584 <2649968259@qq.com>
2025-06-09 14:57:31 +0800

    Add files via upload

--- UDPClient.java
+++ UDPClient.java
@@ -1,3 +1,8 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class UDPClient {
     public static void main(String[] args) {
         if (args.length != 3) {
@@ -8,5 +13,15 @@ public class UDPClient {
         String serverHost = args[0];
         int serverPort = Integer.parseInt(args[1]);
         String fileList = args[2];
+        try {
+            // 读取文件列表
+            List<String> filenames = Files.readAllLines(Paths.get(fileList));
+
+
+        } catch (IOException e) {
+
+        }
+
     }
+
 }
