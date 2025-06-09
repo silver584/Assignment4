f48d526fa9590c65ca8eb57866331663f1773e48
silver584 <2649968259@qq.com>
2025-06-09 18:43:09 +0800

    Add files via upload

--- UDPServer.java
+++ UDPServer.java
@@ -1,6 +1,15 @@
 import java.net.DatagramSocket;
 
+
+
+
 public class UDPServer {
+
+
+    private static final int MIN_DATA_PORT = 50000;
+    private static final int MAX_DATA_PORT = 51000;
+    private static final int MAX_PACKET_SIZE = 65536;
+
     public static void main(String[] args) {
         if (args.length == 1){
             System.out.println("Usage: java UDPServer <port>");
@@ -10,8 +19,11 @@ public class UDPServer {
 
         int port = Integer.parseInt(args[0]);
 
+        try (DatagramSocket serverSocket = new DatagramSocket(port)) {
+            System.out.println("Server started on port " + port);
+            byte[] buffer = new byte[MAX_PACKET_SIZE];
 
-
+        }
 
     }
 }
