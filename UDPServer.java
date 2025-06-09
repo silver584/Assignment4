2e59eb5f83f0bf7467b81ddbd844cba3061c4f1d
silver584 <2649968259@qq.com>
2025-06-09 14:38:59 +0800

    Add files via upload

---
+++ UDPServer.java
@@ -0,0 +1,7 @@
+public class UDPServer {
+    public static void main(String[] args) {
+        if (args.length != 1){
+            System.out.println("Usage: java UDPServer <port>");
+        }
+    }
+}
