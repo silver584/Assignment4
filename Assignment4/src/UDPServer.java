294b4f6f8f6146bafee84698d6dccf5f62c02c64
silver584 <2649968259@qq.com>
2025-06-09 14:52:56 +0800

    Add files via upload

--- Assignment4/src/UDPServer.java
+++ Assignment4/src/UDPServer.java
@@ -1,5 +1,17 @@
+import java.net.DatagramSocket;
+
 public class UDPServer {
     public static void main(String[] args) {
-        
+        if (args.length == 1){
+            System.out.println("Usage: java UDPServer <port>");
+        }
+        String parameter = args[0]; // 获取第一个参数
+        System.out.println("The provided parameter is: " + parameter);
+
+        int port = Integer.parseInt(args[0]);
+
+
+
+
     }
 }
