11e85c34e5ad2001df9e55b3eb0f7897befed2f1
silver584 <2649968259@qq.com>
2025-06-09 14:50:24 +0800

    Add files via upload

--- UDPServer.java
+++ UDPServer.java
@@ -1,7 +1,11 @@
 public class UDPServer {
     public static void main(String[] args) {
-        if (args.length != 1){
+        if (args.length == 1){
             System.out.println("Usage: java UDPServer <port>");
         }
+        String parameter = args[0]; // 获取第一个参数
+        System.out.println("The provided parameter is: " + parameter);
+
+
     }
 }
